048c1c510c21bf76e604fa404474f9584d6c9dd8
0x01athena <brandonng1995919@gmail.com>
2025-09-29 16:14:38 +0900

    feat: update hero section

--- src/sections/section-hero.tsx
+++ src/sections/section-hero.tsx
@@ -5,7 +5,7 @@ import mobileImg from "../assets/mobile_1.png"
 
 export default function SectionHero() {
     return (
-        <header id="hero" className="pt-20 md:pt-40 z-10 relative max-w-screen-xl mx-auto grid lg:grid-cols-2 gap-10 xl:gap-0 items-center px-4 sm:px-8 xl:px-0">
+        <header id="hero" className="pt-10 md:pt-40 z-10 relative max-w-screen-xl mx-auto grid lg:grid-cols-2 gap-10 xl:gap-0 items-center px-4 sm:px-8 xl:px-0">
             <div>
                 <div className="inline-flex rounded-full py-1.5">
                     <div className="rounded-full px-4 py-1.5 text-sm font-semibold bg-[#0B1116]/80 backdrop-blur-[2px] ring-1 ring-inset ring-white/10">
